a8b394030ebc354fc23c62317e3db116b494b55c
jobren95 <jobren95@gmail.com>
2023-04-23 07:56:12 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers of base 10 from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
