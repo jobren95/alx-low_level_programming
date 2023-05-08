b4f8d38e4b9ea271708e01bf0e26248cce2ea9ad
jobren95 <jobren95@gmail.com>
2023-05-08 11:39:07 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,23 +6,19 @@
  * @needle: input
  * Return: Always 0 (Success)
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	
+
 	for (; *haystack != '\0'; haystack++)
-        {
-		
+	{
 		char *l = haystack;
-		
 		char *p = needle;
-		
+
 		while (*l == *p && *p != '\0')
-                {
+		{
 			l++;
 			p++;
 		}
-		
 		if (*p == '\0')
 			return (haystack);
 	}
