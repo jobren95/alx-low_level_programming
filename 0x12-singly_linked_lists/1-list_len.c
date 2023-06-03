390bf87e3780be929776468e155675454dcc4cfe
jobren95 <jobren95@gmail.com>
2023-06-03 05:23:44 -0700

    task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,18 +5,17 @@
  * @h: pointer to the list_t list
  * Return: counts of elements in h
  */
-size_t lists_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t count = 0;
 
 	if (h == NULL)
-	{
 		return (0);
-	}
 
-	for (const list_t *node = h; node != NULL; node = node->next)
+	while (h != NULL)
 	{
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
