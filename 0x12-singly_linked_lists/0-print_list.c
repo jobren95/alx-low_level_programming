3f43f588908ea32ec7494d5f2fe27d885d9bbb0b
jobren95 <jobren95@gmail.com>
2023-06-03 04:21:17 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,12 @@ size_t print_list(const list_t *h)
 	size_t node_num = 1;
 
 	if (h == NULL)
-		return(0);
+		return (0);
 
 	while (h->next != NULL)
 
 	{
-		if (h->str ==NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
