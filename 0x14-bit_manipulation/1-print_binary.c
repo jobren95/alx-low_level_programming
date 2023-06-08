9e93bdff0f323a351f25db0aa2c3e09125ce1b32
jobren95 <jobren95@gmail.com>
2023-06-08 14:01:21 -0700

    task0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary
+ * @n: counts to print in binary
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int a, num = 0;
+	unsigned long int new;
+
+	for (a = 63; a >= 0; a--)
+	{
+		new = n >> a;
+
+		if (new & 1)
+		{
+			_putchar('1');
+			num++;
+
+		}
+		else if (num)
+			_putchar('0');
+	}
+	if (!num)
+		_putchar('0')
